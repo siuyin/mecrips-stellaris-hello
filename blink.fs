bfb00bb6695f9b4cd548c6853fd65313713973a9
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-11-10 10:46:53 +0800

    Implemented led toggle in blink.

--- blink.fs
+++ blink.fs
@@ -1,10 +1,12 @@
 \ LED on PA5.
 
-compiletoflash
+forgetram
+compiletoram
 
 \ register defines
 $48000000 constant GPIOA		 
 GPIOA $0 + constant GPIOA_MODER
+GPIOA $14 + constant GPIOA_ODR ( GPIO port output data register ) 
 GPIOA $18 + constant GPIOA_BSRR
 
 
@@ -15,13 +17,19 @@ GPIOA $18 + constant GPIOA_BSRR
 : led.on   %1  5 lshift GPIOA_BSRR bis! ;	
 
 : led.off  1 21 lshift GPIOA_BSRR bis! ; 
+
+: ledTgl ( -- )
+    1 5 lshift GPIOA_ODR bit@ if
+        led.off
+    else
+        led.on
+    then
+;
  
 : blink		
     initLED
     begin
-        led.on
-        half-second-delay
-        led.off
+        ledTgl
         half-second-delay
         key? 
     until
